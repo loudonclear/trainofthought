a46a0759b818fd7469e1198fdfb447a351e8cb9f
Isaiah <flyanim@gmail.com>
2019-02-23 12:50:22 -0500

    Version 0.1
    Basic Controls and Decision Making

---
+++ Train Of Thought Core/Assets/_Scripts/DecisionScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecisionScript : MonoBehaviour {
+
+	public enum trackDirection {LeftSplit, Fork, RightSplit};
+	private GameManagerScript sc;
+	private Text notification;
+	private Text option1Text;
+	private Text option2Text;
+	public GameObject choice1;
+	public GameObject choice2;
+
+	void Start () {
+		sc = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+		notification = GameObject.Find("Notification").GetComponent<Text>();
+		option1Text = GameObject.Find("Option 1").GetComponent<Text>();
+		option2Text = GameObject.Find("Option 2").GetComponent<Text>();
+		notification.text = "";
+		option1Text.text = choice1.GetComponent<ChoiceScript>().description;
+		option2Text.text = choice2.GetComponent<ChoiceScript>().description;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (sc.decision == 1) 
+		{
+			notification.text = "You chose " + choice1.GetComponent<ChoiceScript>().description;
+			Destroy(this.gameObject);
+		} else if (sc.decision == 2)
+		{
+			notification.text = "You're dead";
+			Destroy(this.gameObject);
+		} else if (sc.decision == 3)
+		{
+			notification.text = "You chose " + choice2.GetComponent<ChoiceScript>().description;
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Train Of Thought Core/Assets/_Scripts/GameManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+	private float swipeDirectionH = 0f;
+	private float swipeDirectionV = 0f;
+	private Vector3 defaultState;
+	public Vector3 leftState;
+	public Vector3 upState;
+	public Vector3 rightState;
+	public float swipeThreshold = 0f;
+	[HideInInspector]
+	public int decision = 0;
+	public GameObject[] decisions;
+	public float updateDecisionDelay = 2f;
+	private bool decisionMade = false;
+	// Use this for initialization
+
+	void Start() {
+		defaultState.Set(transform.position.x, transform.position.y, transform.position.z);
+		Instantiate(decisions[Random.Range(0, decisions.Length)]);
+	}
+
+	void Update() {
+		swipeDirectionH = Input.GetAxis("Horizontal");
+		swipeDirectionV = Input.GetAxis("Vertical");
+	}
+
+	void FixedUpdate () {
+		if (!decisionMade) {
+			if (swipeDirectionH < swipeThreshold) 
+			{
+				decision = 1;
+				transform.position = leftState;
+				StartCoroutine("updateDecision");
+				decisionMade = true;
+			} else if (swipeDirectionH > swipeThreshold) 
+			{
+				decision = 3;
+				transform.position = rightState;
+				StartCoroutine("updateDecision");
+				decisionMade = true;
+			} else if (swipeDirectionV > swipeThreshold) 
+			{
+				decision = 2;
+				transform.position = upState;
+				StartCoroutine("updateDecision");
+				decisionMade = true;
+			} else 
+			{
+				transform.position = defaultState;
+			}
+		}
+	}
+
+	IEnumerator updateDecision() {
+		yield return new WaitForSeconds(updateDecisionDelay);
+		decisionMade = false;
+		decision = 0;
+		Instantiate(decisions[Random.Range(0, decisions.Length)]);
+	}
+}
