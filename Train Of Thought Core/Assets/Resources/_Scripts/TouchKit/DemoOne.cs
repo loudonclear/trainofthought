8381cb45c1f4256e47ba15cde2bbbf6c0f529a53
Isaiah <flyanim@gmail.com>
2019-05-04 14:50:51 -0400

    Changed Swipe Min Max

--- Train Of Thought Core/Assets/Resources/_Scripts/TouchKit/DemoOne.cs
+++ Train Of Thought Core/Assets/Resources/_Scripts/TouchKit/DemoOne.cs
@@ -4,12 +4,13 @@ using System.Collections;
 
 public class DemoOne : MonoBehaviour
 {
-	private Vector2 _scrollPosition; // for the scroll view
-	public AnimationCurve touchPadInputCurve = AnimationCurve.Linear( 0.0f, 0.0f, 1.0f, 1.0f );
+    //private Vector2 _scrollPosition; // for the scroll view
+    //public AnimationCurve touchPadInputCurve = AnimationCurve.Linear( 0.0f, 0.0f, 1.0f, 1.0f );
+    public Vector2 swipeMinMax = new Vector2(1, 10);
 
     private void Start()
     {
-        var recognizer = new TKSwipeRecognizer();
+        var recognizer = new TKSwipeRecognizer(swipeMinMax.x, swipeMinMax.y);
         recognizer.gestureRecognizedEvent += (r) =>
         {
             Debug.Log("swipe recognizer fired: " + r);
